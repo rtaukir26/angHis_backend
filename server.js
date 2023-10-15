60066b9db95ffae664f1d47d6b70cc0a1ecb1d98
rtaukir26 <111485623+rtaukir26@users.noreply.github.com>
2023-10-15 14:34:08 +0530

    db connection

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
-const app = require("./app")
+const app = require("./app");
+const connectDb = require("./config/dataBase");
 const dotenv = require("dotenv");
 dotenv.config({ path: "config/config.env" });
 
+//connecting to Db
+connectDb();
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on ${process.env.PORT}`);
 });
